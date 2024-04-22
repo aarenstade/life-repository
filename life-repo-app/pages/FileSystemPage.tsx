59dfaa1f2f3a611b9e94df31a3a28dd4125c281c
aaren <aarenstade@gmail.com>
2024-04-21 23:54:17 -0700

    Add FileSystemPage component

---
+++ life-repo-app/pages/FileSystemPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { View } from "react-native";
+
+interface FileSystemPageProps {}
+
+const FileSystemPage: FC<FileSystemPageProps> = () => {
+  return <View></View>;
+};
+
+export default FileSystemPage;
