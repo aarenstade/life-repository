0f502831a905e9812c1d7b7efa4818ffe22592a7
aaren <aarenstade@gmail.com>
2024-04-21 20:08:43 -0700

    Refactor navigation and add config page

--- life-repo-app/App.tsx
+++ life-repo-app/App.tsx
@@ -1,19 +1,52 @@
-import { View, Text, Button } from "react-native";
-import React, { useState } from "react";
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { NativeStackScreenProps, createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomePage from "./pages/HomePage";
+import { TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import ConfigPage from "./pages/ConfigPage";
+import useConfig from "./hooks/useConfig";
+import LoadingIndicator from "./components/LoadingIndicator";
+
+const Stack = createNativeStackNavigator();
+
+export type PageParams = {
+  Home: undefined;
+  Settings: undefined;
+};
+
+export type HomePageProps = NativeStackScreenProps<PageParams, "Home">;
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    console.log("Hello World");
-    setCount(count + 1);
-  };
+  const config = useConfig();
 
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text>Awesome</Text>
-      <Button title='Click me' onPress={handleClick} />
-      <Text>Click Count: {count}</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={HomePage}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <View className='flex flex-row space-x-4'>
+                {config.loading ? (
+                  <LoadingIndicator style={{ marginRight: 10 }} />
+                ) : config.connected ? (
+                  <Feather name='check' size={24} color='green' />
+                ) : (
+                  <Feather name='alert-circle' size={24} color='red' />
+                )}
+                <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
+                  <Feather name='settings' size={24} color='black' />
+                </TouchableOpacity>
+              </View>
+            ),
+          })}
+        />
+        <Stack.Screen name='Settings' component={ConfigPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
