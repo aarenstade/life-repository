29868cd52f78bef1529b1feff06fabad7a6ae4e2
aaren <aarenstade@gmail.com>
2024-04-21 23:54:10 -0700

    Add CardButton and TranscribeTextArea components to HomePage

--- life-repo-app/pages/HomePage.tsx
+++ life-repo-app/pages/HomePage.tsx
@@ -1,9 +1,22 @@
 import React, { FC } from "react";
 import { View } from "react-native";
-import { HomePageProps } from "../App";
+import { HomePageProps, PageParams } from "../App";
+import CardButton from "../components/CardButton";
+import { Feather } from "@expo/vector-icons";
+import TranscribeTextArea from "../components/inputs/TranscribeTextArea";
 
 const HomePage: FC<HomePageProps> = ({ navigation }) => {
-  return <View className='flex flex-col justify-center items-center m-4'></View>;
+  const navigateToPage = (page: keyof PageParams) => {
+    navigation.navigate(page);
+  };
+
+  return (
+    <View className='flex flex-col justify-center items-center space-y-4 mt-4 w-full'>
+      <CardButton icon={<Feather name='file' size={24} color='black' />} title='File System' onClick={() => navigateToPage("file_system")} />
+      <CardButton icon={<Feather name='plus-circle' size={24} color='black' />} title='Add Files' onClick={() => navigateToPage("add_files")} />
+      <TranscribeTextArea />
+    </View>
+  );
 };
 
 export default HomePage;
