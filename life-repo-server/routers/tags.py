a6d8c59883a8986672c08be3ce558682d017e2e0
aaren <aarenstade@gmail.com>
2024-04-30 21:53:36 -0700

    Add tags router with autocomplete endpoint

---
+++ life-repo-server/routers/tags.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException, Request
+from typing import List
+
+router = APIRouter()
+
+
+@router.get("/autocomplete", response_model=List[str])
+async def get_tag_annotations(request: Request, search: str):
+    print(request.app.state)
+    db = request.app.state.db
+
+    try:
+        tags = db.fetch_all(
+            "SELECT tag FROM tags WHERE tag LIKE :search_tag ORDER BY tag ASC LIMIT 10",
+            {"search_tag": f"%{search}%"},
+        )
+        if not tags:
+            raise HTTPException(status_code=404, detail="No tags found")
+        return [tag["tag"] for tag in tags]
+    finally:
+        db.disconnect()
