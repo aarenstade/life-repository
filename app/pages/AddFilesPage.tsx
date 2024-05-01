f58a5211f93eda79204ef3ffdd09bd3ff58cab37
aaren <aarenstade@gmail.com>
2024-04-30 19:37:21 -0700

    Refactor AddFilesPage component

--- app/pages/AddFilesPage.tsx
+++ app/pages/AddFilesPage.tsx
@@ -1,68 +1,16 @@
 import { FC, useState } from "react";
-import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
-import ImageCameraPicker from "../components/photos/ImageCameraPicker";
+import { Alert, ScrollView, View, StyleSheet } from "react-native";
 import { AddFilesPageProps } from "../App";
-import FileGrid from "../components/photos/FileGrid";
-import TranscribeTextInput from "../components/inputs/transcribe-inputs/TranscribeTextInput";
-
-interface ChinViewProps {
-  onContinue: () => void;
-  onCancel: () => void;
-  onBack?: () => void;
-}
-
-const ChinView: FC<ChinViewProps> = ({ onContinue, onCancel, onBack }) => {
-  return (
-    <View style={chinViewStyles.chinViewContainer}>
-      <TouchableOpacity onPress={onCancel} style={[chinViewStyles.chinViewButton, chinViewStyles.cancelButton]}>
-        <Text style={chinViewStyles.chinViewButtonText}>Cancel</Text>
-      </TouchableOpacity>
-      {onBack && (
-        <TouchableOpacity onPress={onBack} style={[chinViewStyles.chinViewButton, chinViewStyles.normalButton]}>
-          <Text style={chinViewStyles.chinViewButtonText}>Back</Text>
-        </TouchableOpacity>
-      )}
-      <TouchableOpacity onPress={onContinue} style={[chinViewStyles.chinViewButton, chinViewStyles.normalButton]}>
-        <Text style={chinViewStyles.chinViewButtonText}>Continue</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const chinViewStyles = StyleSheet.create({
-  chinViewContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 10,
-  },
-  chinViewButton: {
-    borderRadius: 20,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginHorizontal: 10,
-  },
-  cancelButton: {
-    backgroundColor: "#A4A4A4", // Gray
-  },
-  backButton: {
-    backgroundColor: "transparent",
-    borderWidth: 1,
-    borderColor: "#007AFF", // Blue outline
-  },
-  normalButton: {
-    backgroundColor: "#007AFF", // Blue
-  },
-  chinViewButtonText: {
-    color: "white",
-    textAlign: "center",
-  },
-});
+import FilePreviewGrid from "../components/media/FilePreviewGrid";
+import { FileGroupAnnotationView, FileIndividualAnnotationView } from "../views/annotation/FileAnnotationView";
+import MultiStepChinView from "../components/control/MultiStepChinView";
+import SelectImagesView from "../views/selection/SelectImagesView";
 
 const AddFilesPage: FC<AddFilesPageProps> = ({ navigation }) => {
-  const [step, setStep] = useState("select-images");
-  const [images, setImages] = useState<string[]>([]);
+  const [step, setStep] = useState("select-files");
+  const [file_uris, setFileUris] = useState<string[]>([]);
 
-  const steps = ["select-images", "annotate-group", "annotate-images"];
+  const steps = ["select-files", "annotate-group", "annotate-individual"];
 
   const [groupData, setGroupData] = useState({
     title: "",
@@ -85,7 +33,7 @@ const AddFilesPage: FC<AddFilesPageProps> = ({ navigation }) => {
   };
 
   const handleCancel = () => {
-    if (images.length > 0) {
+    if (file_uris.length > 0) {
       Alert.alert(
         "Confirm",
         "Are you sure you want to cancel? All unsaved changes will be lost.",
@@ -104,111 +52,77 @@ const AddFilesPage: FC<AddFilesPageProps> = ({ navigation }) => {
   };
 
   let view;
-  const styles = StyleSheet.create({
-    selectImagesContainer: {
-      flex: 1,
-      justifyContent: "space-between",
-    },
-    annotateGroupContainer: {
-      display: "flex",
-      flexDirection: "column",
-      padding: 20,
-      gap: 20,
-    },
-    groupDetailsText: {
-      fontSize: 20,
-      fontWeight: "bold",
-      marginBottom: 16,
-    },
-    input: {
-      height: 40,
-      borderColor: "gray",
-      borderWidth: 1,
-      marginBottom: 20,
-    },
-    inputMargin: {
-      marginBottom: 20,
-    },
-  });
 
-  if (step === "select-images") {
+  if (step === "select-files") {
+    view = <SelectImagesView images={file_uris} setImages={setFileUris} />;
+  }
+
+  if (step == "annotate-group") {
     view = (
-      <View style={styles.selectImagesContainer}>
-        <ImageCameraPicker images={images} onImagesChanged={setImages} />
-      </View>
+      <FileGroupAnnotationView
+        file_uris={file_uris}
+        onFileUrisChange={setFileUris}
+        onPreviousStep={handlePreviousStep}
+        onNextStep={handleNextStep}
+        hideFilePreviewGrid
+      />
     );
   }
 
-  if (step === "annotate-group") {
-    view = (
-      <View style={styles.annotateGroupContainer}>
-        <Text style={styles.groupDetailsText}>Group Details</Text>
-        <TranscribeTextInput
-          initialText={groupData.title}
-          onChangeText={(text) => setGroupData({ ...groupData, title: text })}
-          placeholder='Enter title'
-        />
-        <TranscribeTextInput
-          initialText={groupData.description}
-          onChangeText={(text) => setGroupData({ ...groupData, description: text })}
-          multiline
-          placeholder='Enter description'
-        />
-        <TranscribeTextInput
-          initialText={groupData.tags.join(",")}
-          onChangeText={(text) => setGroupData({ ...groupData, tags: text.split(",") })}
-          placeholder='Enter tags, separated by commas'
-        />
-      </View>
+  if (step == "annotate-individual") {
+    return (
+      <FileIndividualAnnotationView
+        file_uris={file_uris}
+        onFileUrisChange={setFileUris}
+        onPreviousStep={handlePreviousStep}
+        onNextStep={handleNextStep}
+      />
     );
   }
-
-  if (step == "annotate-images") {
-    view = <View></View>;
-  }
-  const mainStyles = StyleSheet.create({
-    container: {
-      flexGrow: 1,
-      paddingVertical: 16,
-      paddingHorizontal: 16,
-      flexDirection: "column",
-      justifyContent: "space-between",
-      height: "100%",
-    },
-    buttonContainer: {
-      flexGrow: 1,
-      marginTop: 8,
-    },
-    button: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 8,
-      borderRadius: 8,
-      elevation: 4,
-    },
-    continueButton: {
-      backgroundColor: "#007bff",
-    },
-    cancelButton: {
-      backgroundColor: "#6c757d",
-    },
-    buttonText: {
-      color: "#ffffff",
-      fontWeight: "bold",
-      textAlign: "center",
-    },
-  });
 
   return (
     <View style={mainStyles.container}>
       <ScrollView>
         {view}
-        {images && <FileGrid files_uris={images} onFileUrisChange={setImages} />}
+        {file_uris && <FilePreviewGrid files_uris={file_uris} onFileUrisChange={setFileUris} />}
       </ScrollView>
-      <ChinView onContinue={handleNextStep} onCancel={handleCancel} onBack={steps.indexOf(step) > 0 ? handlePreviousStep : undefined} />
+      <MultiStepChinView onContinue={handleNextStep} onCancel={handleCancel} onBack={steps.indexOf(step) > 0 ? handlePreviousStep : undefined} />
     </View>
   );
 };
+
+const mainStyles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    height: "100%",
+  },
+  buttonContainer: {
+    flexGrow: 1,
+    marginTop: 8,
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    borderRadius: 8,
+    elevation: 4,
+  },
+  continueButton: {
+    backgroundColor: "#007bff",
+  },
+  cancelButton: {
+    backgroundColor: "#6c757d",
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
 export default AddFilesPage;
