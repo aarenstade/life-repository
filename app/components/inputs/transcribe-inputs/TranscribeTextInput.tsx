27ce2ae75da81c6c8dc03af8a4a953da21b2685c
aaren <aarenstade@gmail.com>
2024-04-30 23:42:54 -0700

    Refactor TranscribeTextInput component to support full screen mode

--- app/components/inputs/transcribe-inputs/TranscribeTextInput.tsx
+++ app/components/inputs/transcribe-inputs/TranscribeTextInput.tsx
@@ -1,14 +1,36 @@
-import { FC } from "react";
-import { useState } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { FC, useState } from "react";
+import { TextInput, View, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
 import RecordIcon from "./RecordIcon";
 
 interface TranscribeTextInputProps {
   initialText?: string;
+  insetTop?: number;
+  insetBottom?: number;
+  insetLeft?: number;
+  insetRight?: number;
+  noFullScreen?: boolean;
   [inputProps: string]: any;
 }
 
-const TranscribeTextInput: FC<TranscribeTextInputProps> = ({ initialText, ...rest }) => {
+const TranscribeTextInput: FC<TranscribeTextInputProps> = ({
+  initialText,
+  insetTop = 0,
+  insetBottom = 0,
+  insetLeft = 0,
+  insetRight = 0,
+  noFullScreen = false,
+  ...rest
+}) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const toggleFullScreen = () => setIsFullScreen(!isFullScreen);
+
+  const { width, height } = Dimensions.get("window");
+  const insets = useSafeAreaInsets();
+
+  const isMultiLine = rest.multiline ?? false;
+
   const handleNewTranscriptText = (transcript: string) => {
     if (rest.value) {
       if (rest.onChangeText) rest.onChangeText(rest.value + "\n" + transcript);
@@ -17,18 +39,23 @@ const TranscribeTextInput: FC<TranscribeTextInputProps> = ({ initialText, ...res
     }
   };
 
-  const isMultiLine = rest.multiline ?? false;
-
   const styles = StyleSheet.create({
     container: {
-      position: "relative",
-      width: "100%",
+      position: isFullScreen ? "absolute" : "relative",
+      width: isFullScreen ? width - (insetLeft + insetRight) : "100%",
+      height: isFullScreen ? height - (insets.top + insetTop + insets.bottom + insetBottom) : isMultiLine ? 150 : 40,
+      zIndex: isFullScreen ? 100 : 1,
+      top: isFullScreen ? insetTop : undefined,
+      left: isFullScreen ? insetLeft : undefined,
+      right: isFullScreen ? insetRight : undefined,
+      bottom: isFullScreen ? insetBottom : undefined,
+      backgroundColor: "white",
     },
     input: {
       width: "100%",
-      minHeight: isMultiLine ? 150 : 40,
+      height: isFullScreen ? height - (insets.top + insetTop + insets.bottom + insetBottom) : isMultiLine ? 150 : 40,
       padding: 10,
-      paddingBottom: isMultiLine ? 50 : 10, // Make space for the record button if multiline
+      paddingBottom: isMultiLine ? 50 : 10,
       borderColor: "gray",
       borderWidth: 1,
       borderRadius: 5,
@@ -36,12 +63,24 @@ const TranscribeTextInput: FC<TranscribeTextInputProps> = ({ initialText, ...res
     recordButton: {
       position: "absolute",
       right: 10,
-      bottom: 10,
+      top: isFullScreen ? 50 : "auto",
+      bottom: isFullScreen ? 0 : 10,
+    },
+    fullscreenButton: {
+      position: "absolute",
+      right: 10,
+      top: 10,
+      zIndex: 20,
     },
   });
 
   return (
     <View style={styles.container}>
+      {rest.multiline && !noFullScreen && (
+        <TouchableOpacity style={styles.fullscreenButton} onPress={toggleFullScreen}>
+          <Feather name={isFullScreen ? "minimize" : "maximize"} size={20} color='black' />
+        </TouchableOpacity>
+      )}
       <TextInput value={rest.value || ""} onChangeText={rest.onChangeText} placeholder='Write something here...' style={styles.input} {...rest} />
       <View style={styles.recordButton}>
         <RecordIcon onTranscriptionComplete={handleNewTranscriptText} />
@@ -49,4 +88,6 @@ const TranscribeTextInput: FC<TranscribeTextInputProps> = ({ initialText, ...res
     </View>
   );
 };
+
 export default TranscribeTextInput;
+//
