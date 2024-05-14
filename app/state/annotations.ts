4db3be2d703f32ced48ee1ed9253fc3cf0f47e9b
aaren <aarenstade@gmail.com>
2024-05-13 22:59:37 -0700

    Add Zustand middleware for persisting annotation drafts

--- app/state/annotations.ts
+++ app/state/annotations.ts
@@ -1,16 +1,27 @@
 import { create } from "zustand";
-import { AnnotationFlowType, AnnotationGroup, BaseAnnotationGroup, FileAnnotation, Tag } from "../types/annotation";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AnnotationFlowType, AnnotationGroup, FileAnnotation, Tag } from "../types/annotation";
 import shortid from "shortid";
+import { utcNow } from "../utilities/general";
 
-interface AnnotationGroupsState {
-  draftGroups: BaseAnnotationGroup[];
-  setDraftGroups: (draftGroups: BaseAnnotationGroup[]) => void;
+interface AnnotationDraftsState {
+  draftGroups: AnnotationGroup[];
+  setDraftGroups: (draftGroups: AnnotationGroup[]) => void;
 }
 
-export const useAnnotationGroups = create<AnnotationGroupsState>((set) => ({
-  draftGroups: [],
-  setDraftGroups: (draftGroups) => set({ draftGroups }),
-}));
+export const useAnnotationDrafts = create<AnnotationDraftsState>()(
+  persist(
+    (set) => ({
+      draftGroups: [],
+      setDraftGroups: (draftGroups) => set({ draftGroups }),
+    }),
+    {
+      name: "draft-annotation-groups",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
 
 interface ActiveAnnotationState {
   step: string;
@@ -24,26 +35,31 @@ interface ActiveAnnotationState {
   removeFile: (fileUri: string) => void;
 }
 
-const defaultGroup: AnnotationGroup = {
-  group_id: shortid.generate(),
-  title: "",
-  description: "",
-  tags: [],
-  num_files: 0,
-  flow_type: "individual-then-group",
-  created_at: "",
-  updated_at: "",
-  files: [],
-};
-
-export const useActiveAnnotation = create<ActiveAnnotationState>((set) => ({
-  group: defaultGroup,
-  setGroup: (group) => set({ group }),
-  setGroupTags: (tags) => set((state) => ({ group: { ...state.group, tags } })),
-  setFlowType: (flowType) => set((state) => ({ group: { ...state.group, flow_type: flowType } })),
-  setFiles: (files) => set((state) => ({ group: { ...state.group, files } })),
-  updateFile: (file) => set((state) => ({ group: { ...state.group, files: state.group.files.map((f) => (f.uri === file.uri ? file : f)) } })),
-  removeFile: (fileUri) => set((state) => ({ group: { ...state.group, files: state.group.files.filter((file) => file.uri !== fileUri) } })),
-  step: "add-type",
-  setStep: (step) => set({ step }),
-}));
+export const useActiveAnnotation = create<ActiveAnnotationState>()(
+  persist(
+    (set) => ({
+      group: {
+        group_id: shortid.generate(),
+        title: "Untitled Group",
+        description: "",
+        tags: [],
+        flow_type: "individual-then-group",
+        created_at: utcNow(),
+        updated_at: utcNow(),
+        files: [],
+      },
+      setGroup: (group) => set({ group }),
+      setGroupTags: (tags) => set((state) => ({ group: { ...state.group, tags } })),
+      setFlowType: (flowType) => set((state) => ({ group: { ...state.group, flow_type: flowType } })),
+      setFiles: (files) => set((state) => ({ group: { ...state.group, files } })),
+      updateFile: (file) => set((state) => ({ group: { ...state.group, files: state.group.files.map((f) => (f.uri === file.uri ? file : f)) } })),
+      removeFile: (fileUri) => set((state) => ({ group: { ...state.group, files: state.group.files.filter((file) => file.uri !== fileUri) } })),
+      step: "add-type",
+      setStep: (step) => set({ step }),
+    }),
+    {
+      name: "active-annotation",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
