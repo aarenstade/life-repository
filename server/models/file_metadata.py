4cf1a1916aa439c30703c874c08c7e7ce7ab5225
aaren <aarenstade@gmail.com>
2024-05-19 14:14:19 -0700

    Refactor file metadata classes to use optional fields

--- server/models/file_metadata.py
+++ server/models/file_metadata.py
@@ -4,48 +4,53 @@ from datetime import datetime
 
 
 class FileMetadata(BaseModel):
-    size: int
-    created_at: datetime
-    modified_at: datetime
+    size: Optional[int] = None
+    created_at: Optional[datetime] = None
+    modified_at: Optional[datetime] = None
 
 
-class ImageMetadata(BaseModel):
+class ImageMetadata(FileMetadata):
     width: Optional[int] = None
     height: Optional[int] = None
     color_mode: Optional[str] = None
     format: Optional[str] = None
+    properties: Optional[dict] = None
+    xmp_data: Optional[dict] = None
+    location: Optional[tuple[float, float]] = None
 
 
-class VideoMetadata(BaseModel):
-    duration: Optional[int] = None
+class VideoMetadata(FileMetadata):
+    duration: Optional[float] = None
     width: Optional[int] = None
     height: Optional[int] = None
     framerate: Optional[float] = None
     codec: Optional[str] = None
     bitrate: Optional[int] = None
+    properties: Optional[dict] = None
+    location: Optional[tuple[float, float]] = None
 
 
-class TextMetadata(BaseModel):
+class TextMetadata(FileMetadata):
     num_words: Optional[int] = None
     language: Optional[str] = None
     encoding: Optional[str] = None
 
 
-class AudioMetadata(BaseModel):
+class AudioMetadata(FileMetadata):
     bitrate: Optional[int] = None
-    duration: Optional[int] = None
+    duration: Optional[float] = None
     sample_rate: Optional[int] = None
     channels: Optional[int] = None
     codec: Optional[str] = None
 
 
-class ArchiveMetadata(BaseModel):
+class ArchiveMetadata(FileMetadata):
     num_files: Optional[int] = None
     compression_type: Optional[str] = None
     encrypted: Optional[bool] = None
 
 
-class DocumentMetadata(BaseModel):
+class DocumentMetadata(FileMetadata):
     num_pages: Optional[int] = None
     author: Optional[str] = None
     title: Optional[str] = None
