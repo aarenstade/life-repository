dd014d2d5399034da22fa4c7706f9cef0ffd1374
aaren <aarenstade@gmail.com>
2024-05-19 14:14:46 -0700

    Refactor image metadata extraction code to utilize Apple native imageIO APIs

--- server/services/files/metadata_extraction/image_metadata_extractor.py
+++ server/services/files/metadata_extraction/image_metadata_extractor.py
@@ -1,16 +1,15 @@
 import os
-from typing import Any, Dict
+from typing import Any, Dict, Tuple, Union
 
 from PIL import Image
-
-from PIL import Image
-import exifread
-import pyheif
-import pyexiv2
-
-
 from models.file_metadata import ImageMetadata
+
 from services.files.metadata_extraction.base import MetadataExtractor
+from services.files.metadata_extraction.image_metadata_functions import (
+    get_image_location,
+    get_image_properties,
+    get_image_xmp_metadata,
+)
 
 
 class ImageMetadataExtractor(MetadataExtractor):
@@ -18,67 +17,74 @@ class ImageMetadataExtractor(MetadataExtractor):
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"File not found: {file_path}")
 
+        width = None
+        height = None
+        format = None
+        color_mode = None
+
         try:
             image = Image.open(file_path)
-            resolution = image.size
             color_mode = image.mode
+            image_size = image.size
             format = image.format
+            width = image_size[0] if image_size else None
+            height = image_size[1] if image_size else None
         except Exception as e:
-            raise ValueError(f"Error opening image file: {e}")
-
-        try:
-            size = os.path.getsize(file_path)
-        except OSError as e:
-            raise ValueError(f"Error getting file size: {e}")
-
-        try:
-            created_at = os.path.getctime(file_path)
-        except OSError as e:
-            raise ValueError(f"Error getting file creation time: {e}")
-
-        try:
-            modified_at = os.path.getmtime(file_path)
-        except OSError as e:
-            raise ValueError(f"Error getting file modification time: {e}")
-
-        exif_data = self.extract_exif_data(file_path)
-        heif_data = self.extract_heif_data(file_path)
-        xmp_data = self.extract_xmp_data(file_path)
+            print(f"Error opening image file: {e}")
 
         return ImageMetadata(
-            size=size,
-            created_at=created_at,
-            modified_at=modified_at,
-            resolution=resolution,
-            color_mode=color_mode,
-            format=format,
-            exif_data=exif_data,
-            heif_data=heif_data,
-            xmp_data=xmp_data,
+            width=width or None,
+            height=height or None,
+            format=format or None,
+            color_mode=color_mode or None,
+            size=self._get_file_size(file_path),
+            created_at=self._get_file_creation_time(file_path),
+            modified_at=self._get_file_modification_time(file_path),
+            properties=self.extract_base_properties(file_path),
+            xmp_data=self.extract_xmp_properties(file_path),
+            location=self.extract_image_location(file_path),
         )
 
-    def extract_exif_data(self, file_path: str) -> Dict[str, Any]:
+    def _get_file_size(self, file_path: str) -> int:
         try:
-            with open(file_path, "rb") as f:
-                tags = exifread.process_file(f)
-            return {tag: str(value) for tag, value in tags.items()}
+            return os.path.getsize(file_path)
         except Exception as e:
-            raise ValueError(f"Error extracting EXIF data: {e}")
+            print(f"Error getting file size: {e}")
+            return 0
 
-    def extract_heif_data(self, file_path: str) -> Dict[str, Any]:
-        if not file_path.lower().endswith(".heic"):
+    def _get_file_creation_time(self, file_path: str) -> float:
+        try:
+            return os.path.getctime(file_path)
+        except Exception as e:
+            print(f"Error getting file creation time: {e}")
+            return 0.0
+
+    def _get_file_modification_time(self, file_path: str) -> float:
+        try:
+            return os.path.getmtime(file_path)
+        except Exception as e:
+            print(f"Error getting file modification time: {e}")
+            return 0.0
+
+    def extract_base_properties(self, file_path: str) -> Dict[str, Any]:
+        try:
+            return get_image_properties(file_path)
+        except Exception as e:
+            print(f"Error extracting base properties: {e}")
             return {}
-        try:
-            heif_file = pyheif.read(file_path)
-            metadata = heif_file.metadata or []
-            return {meta["type"]: meta["data"] for meta in metadata}
-        except Exception as e:
-            raise ValueError(f"Error extracting HEIF data: {e}")
 
-    def extract_xmp_data(self, file_path: str) -> Dict[str, Any]:
+    def extract_xmp_properties(self, file_path: str) -> Dict[str, Any]:
         try:
-            with pyexiv2.Image(file_path) as img:
-                xmp_data = img.read_xmp()
-            return xmp_data
+            return get_image_xmp_metadata(file_path)
         except Exception as e:
-            raise ValueError(f"Error extracting XMP data: {e}")
+            print(f"Error extracting XMP properties: {e}")
+            return {}
+
+    def extract_image_location(
+        self, file_path: str
+    ) -> Union[Tuple[float, float], None]:
+        try:
+            return get_image_location(file_path)
+        except Exception as e:
+            print(f"Error extracting image location: {e}")
+            return None
