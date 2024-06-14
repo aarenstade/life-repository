e30bda494c5bc8d23c6ef529de5fd1e420030ac5
aaren <aarenstade@gmail.com>
2024-06-13 20:25:01 -0700

    base files and file_metadata tables

--- supabase/migrations/20240519025705_base_schema.sql
+++ supabase/migrations/20240519025705_base_schema.sql
@@ -2,66 +2,43 @@ CREATE TYPE file_type AS ENUM ('IMAGE', 'VIDEO', 'TEXT', 'AUDIO', 'ARCHIVE', 'DO
 
 -- file tables
 
-CREATE TABLE image_metadata (
-    id SERIAL PRIMARY KEY,
-    width INTEGER,
-    height INTEGER,
-    color_mode VARCHAR(50),
-    format VARCHAR(50)
-);
-
-CREATE TABLE video_metadata (
-    id SERIAL PRIMARY KEY,
-    duration INTEGER,
-    width INTEGER,
-    height INTEGER,
-    framerate DECIMAL,
-    codec VARCHAR(50),
-    bitrate INTEGER
-);
-
-CREATE TABLE text_metadata (
-    id SERIAL PRIMARY KEY,
-    num_words INTEGER,
-    language VARCHAR(50),
-    encoding VARCHAR(50)
-);
-
-CREATE TABLE audio_metadata (
-    id SERIAL PRIMARY KEY,
-    bitrate INTEGER,
-    duration INTEGER,
-    sample_rate INTEGER,
-    channels INTEGER,
-    codec VARCHAR(50)
-);
-
-CREATE TABLE archive_metadata (
-    id SERIAL PRIMARY KEY,
-    num_files INTEGER,
-    compression_type VARCHAR(50),
-    encrypted BOOLEAN
-);
-
-CREATE TABLE document_metadata (
-    id SERIAL PRIMARY KEY,
-    num_pages INTEGER,
-    author VARCHAR(255),
-    title VARCHAR(255),
-    language VARCHAR(50)
-);
-
 CREATE TABLE file_metadata (
-    id SERIAL PRIMARY KEY,
-    size BIGINT,
+    id UUID NOT NULL DEFAULT gen_random_uuid (),
+    file_id UUID NOT NULL,
+    size NUMERIC,
     created_at TIMESTAMP NOT NULL,
     modified_at TIMESTAMP NOT NULL,
-    image_metadata_id INTEGER REFERENCES image_metadata(id),
-    video_metadata_id INTEGER REFERENCES video_metadata(id),
-    text_metadata_id INTEGER REFERENCES text_metadata(id),
-    audio_metadata_id INTEGER REFERENCES audio_metadata(id),
-    archive_metadata_id INTEGER REFERENCES archive_metadata(id),
-    document_metadata_id INTEGER REFERENCES document_metadata(id)
+    image_width NUMERIC,
+    image_height NUMERIC,
+    image_color_mode VARCHAR(50),
+    image_format VARCHAR(50),
+    image_properties JSONB,
+    image_xmp_data JSONB,
+    image_location JSONB,
+    video_duration NUMERIC,
+    video_width NUMERIC,
+    video_height NUMERIC,
+    video_framerate DECIMAL,
+    video_codec VARCHAR(50),
+    video_bitrate NUMERIC,
+    video_properties JSONB,
+    video_location JSONB,
+    text_num_words NUMERIC,
+    text_language VARCHAR(50),
+    text_encoding VARCHAR(50),
+    audio_bitrate NUMERIC,
+    audio_duration NUMERIC,
+    audio_sample_rate NUMERIC,
+    audio_channels NUMERIC,
+    audio_codec VARCHAR(50),
+    archive_num_files NUMERIC,
+    archive_compression_type VARCHAR(50),
+    archive_encrypted BOOLEAN,
+    document_num_pages NUMERIC,
+    document_author VARCHAR(255),
+    document_title VARCHAR(255),
+    document_language VARCHAR(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE files (
@@ -69,6 +46,5 @@ CREATE TABLE files (
     name VARCHAR(255),
     type file_type NOT NULL,
     path VARCHAR(255) NOT NULL,
-    metadata_id INTEGER REFERENCES file_metadata(id),
     PRIMARY KEY (id, path)
 );
